deb22cf87ba39a45ea035ebff4230ac299570245
aaaDrown <1662326564@qq.com>
2023-10-16 10:25:59 +0800

    schema

--- server/handler.py
+++ server/handler.py
@@ -9,10 +9,9 @@ import tornado.autoreload
 import tornado.httpserver
 from os.path import abspath, dirname, join
 from tornado.options import options, parse_command_line
+from core.route import routes,route
 
-
-from model import ServerModel, AdminModel
-from flask import Flask     #导入Flask模块
+from model import ServerModel
 from dotenv import load_dotenv
 import os
 
@@ -22,26 +21,50 @@ api_keys = os.getenv('API_KEYS')
 HOST = os.getenv('HOST')
 PORT = os.getenv('PORT')
 
-print(api_keys)
-#创建Flask的实例对象
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/novel')
-def hello_world():
-    return 'Hello World!'
-
+@route(r"/novel")
 class NovelHandler(tornado.web.RequestHandler):
     #get /novel
     def get(self,model: ServerModel = None):
         novels = model.get_novel_list()
+        self.finish(
+
+        )
 
 
-if __name__ == '__main__':
-    app.run(port=PORT,host=HOST)
+@route(r"/novel/([0-9a-z]{24})")
+class CharacterHandler(tornado.web.RequestHandler):
+    #get /novel/character
+    def get(self,model: ServerModel = None):
+        characters = model.get_character_list()
+        self.finish(
+
+        )
+
+@route(r"/novel/character/chat")
+class ChatHandler(tornado.web.RequestHandler):
+    #get /novel/character/chat
+    def get(self,model: ServerModel = None):
+        characters = model.get_character_list()
+        self.finish(
+
+        )
+
+
+
+if __name__ == "__main__":
+
+
+
+    application = tornado.web.Application(routes(), **dict(
+        debug=options.DEBUG,
+    ))
+    server = tornado.httpserver.HTTPServer(application)
+    server.listen(port=options.SERVER_PORT)
+    server.start()
+    logging.info("Start Success: 0.0.0.0:{}".format(options.SERVER_PORT))
+
+    tornado.ioloop.IOLoop.instance().start()
+
 
 
 
